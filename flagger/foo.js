487ae05823aecbaae764757a45e75ba6d1451731
Daniel Jackoway <danjdel@gmail.com>
2013-05-29 22:52:19 -0700

    testcase foo.js

---
+++ flagger/foo.js
@@ -0,0 +1,22 @@
+
+function a() {
+  // Life, Universe, and Everything
+  try {
+    var answer = 6 * 7;
+  } catch (e) {
+
+  }
+}
+
+
+function b() {
+/* Here is a comment
+spanning multiple
+lines xs*/
+    if (false) {
+        var x = 5;
+        x += 3
+        return x;
+    }
+    return "a";
+}
