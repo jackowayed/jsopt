72411303876d272979ff6da4bc994d62b42baf62
Daniel Jackoway <danjdel@gmail.com>
2013-05-29 22:50:53 -0700

    flagger finds try/catch

---
+++ flagger/flagger.js
@@ -0,0 +1,102 @@
+var esprima = require("esprima");
+var console = require("console");
+var fs = require("fs");
+var exec = require("child_process").exec;
+runData = {};
+content = undefined;
+
+main();
+
+
+function main() {
+    var filename = "../floitsch-downloads/optimizing-for-v8/trace-inlining2.js";
+    content = fs.readFileSync(filename, "utf-8");
+    var tree = esprima.parse(content, { tolerant: true, loc: true, range: true });
+    // On other systems might need to change to ./d8
+    exec("d8 --trace-inlining " + filename, function(error, stdout, stderr) {
+        lines = stdout.split("\n");
+        /* runData.inline is a map from function name to an object containing the 
+         property calledFrom, which is a list of all the functions it has been 
+        called from and flagged. */
+        runData.inline = {};
+        var matchPattern = "Did not inline ([^ ]*) called from ([^ ]*) \((.*)?\).";
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            var result = line.match(matchPattern);
+            if (result) {
+                handleMatch(result);
+            }
+        }
+        findTry(tree);
+    });
+}
+
+function handleMatch(result) {
+    var fcnName = result[1];
+    var callingFcn = result[2];
+    var message = result[3];
+    if (message.indexOf("not inlineable") != -1) {
+        if (!runData.inline[fcnName]) {
+            runData.inline[fcnName] = {};
+        }
+        if (runData.inline[fcnName].callingFcns) {
+            runData.inline[fcnName].callingFcns.push(callingFcn);
+        } else {
+            runData.inline[fcnName].callingFcns = [callingFcn];
+        }
+    }
+}
+
+
+/* Pretty-print object as JSON. */
+function prettyObj(obj) {
+    return JSON.stringify(obj, undefined, 2); // Use 2 spaces for indentation
+}
+
+function isTry(obj, currFcn) {
+    if (obj && obj.type =="TryStatement") {
+        console.log("Offending code: starting at Line " + obj.loc.start.line + " in function " + currFcn);
+        if (runData.inline[currFcn]) {
+            console.log("Try/Catch likely prevented this function from being inlined by V8.");
+        }
+        console.log(content.slice(obj.range[0], obj.range[1]));
+    }
+}
+
+/* Try-catch loops cannot be optimized. */
+function findTry(ast) {
+    traverse(ast, isTry, undefined);
+}
+
+
+/* Big comments in the middle of functions prevent the functions from being inlined */
+function isBigComment(obj, currFcn) {
+    if (obj && obj.type == "FunctionDeclaration") {
+        console.log(content.slice(obj.range[0], obj.range[1]));
+    }
+}
+
+function findBigComment(ast) {
+    traverse(ast, isBigComment, undefined);
+}
+
+// from https://github.com/ariya/esprima/blob/master/examples/findbooleantrap.js
+// Executes visitor on the object and its children (recursively).
+function traverse(object, visitor, currFcn) {
+    var key, child;
+
+    if (visitor.call(null, object, currFcn) === false) {
+        return;
+    }
+    for (key in object) {
+        if (object.hasOwnProperty(key)) {
+            child = object[key];
+            if (typeof child === 'object' && child !== null) {
+                if(child.type == "FunctionDeclaration") {
+                    currFcn = child.id.name
+                }
+                traverse(child, visitor, currFcn);
+            }
+        }
+    }
+}
