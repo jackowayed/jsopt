1851f58bf18c3511af23a1fd48cb0cf6f172fff7
Sophia <swestwood92@gmail.com>
2013-06-08 16:56:00 -0700

    Added ability to instrument variable declarations, with example

--- flagger/foo.js
+++ flagger/foo.js
@@ -1,30 +1,39 @@
 
-function a(onlyParam) {
-  // Life, Universe, and Everything
-  try {
-    var answer = 6 * 7;
-  } catch (e) {
+// function a(onlyParam) {
+//   // Life, Universe, and Everything
+//   try {
+//     var answer = 6 * 7;
+//   } catch (e) {
 
-  }
+//   }
+// }
+
+
+// function b(first, second) {
+// /* Here is a comment
+// spanning multiple
+// lines xs*/
+//     if (false) {
+//         var x = 5;
+//         x += 3
+//         return x;
+//     }
+//     return "a";
+// }
+
+function c() {
+  var peeta = "I am Peeta";
+  var gale = "I am Gale"; 
+  return 3;
 }
+// a("hello")
+// a({})
+// a(null)
+// b("harry", 4)
+// b("another", "something")
 
+// var person = {firstname:"John",lastname:"Doe"};  
+// person.lastname.suffix = "DR";
 
-function b(first, second) {
-/* Here is a comment
-spanning multiple
-lines xs*/
-    if (false) {
-        var x = 5;
-        x += 3
-        return x;
-    }
-    return "a";
-}
-a("hello")
-a({})
-a(null)
-b("harry", 4)
-b("another", "something")
-
-var person = {firstname:"John",lastname:"Doe"};  
-person.lastname.suffix = "DR";
+var katniss = "Hunger games";
+c();
